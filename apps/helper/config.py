373091134f8b9c6faa4826682003c097a2e0324b
Cao Minh Tri <23521635@gm.uit.edu.vn>
2025-09-08 22:35:14 +0700

    Rework logger singleton

--- apps/helper/config.py
+++ apps/helper/config.py
@@ -1,27 +1,28 @@
 import dotenv
 import json
-import threading 
-from helper.logger import LoggerSingleton
+import threading
+from logger import LoggerSingleton
 
 logger = LoggerSingleton().get_instance()
 
+
 class ConfigSingleton:
-    __instance = None 
+    __instance = None
     __lock = threading.Lock()
-    
-    def __new(cls, *args, **kwargs):
+
+    def __new__(cls, *args, **kwargs):
         if not cls.__instance:
             with cls.__lock:
                 if not cls.__instance:
                     cls.__instance = super().__new__(cls)
                     cls.__instance.__initialize(*args, **kwargs)
-                    
-    
+        return cls.__instance
+
     def __initialize(self):
-        #config = dotenv.dotenv_values(".env")
+        #self.config = dotenv.dotenv_values(".env")
         self.__instance = ConfigSingleton()
-        
+
         logger.info(f"Config: {self.__instance.__dict__}")
-        
+
     def get_instance(self):
         return self.__instance
